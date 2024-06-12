6f15eb75d2c0512f69e6eca91102916c40458ebb
mahdi <45118847+mahdiyami@users.noreply.github.com>
2024-06-12 09:51:36 +0330

    fix text style ext color

--- lib/ext/textstyle.dart
+++ lib/ext/textstyle.dart
@@ -1,27 +1,59 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 extension TextStyleExtn on TextStyle {
   ColorScheme cs(BuildContext context) => Theme.of(context).colorScheme;
-  TextStyle primery(BuildContext context) =>
-      copyWith(color: cs(context).primary);
-  TextStyle secondary(BuildContext context) =>
-      copyWith(color: cs(context).secondary);
-  TextStyle tertiary(BuildContext context) =>
-      copyWith(color: cs(context).tertiary);
+  TextStyle primary(BuildContext context) => copyWith(color: cs(context).primary);
+  TextStyle primaryContainer(BuildContext context) => copyWith(color: cs(context).primaryContainer);
+  TextStyle primaryFixed(BuildContext context) => copyWith(color: cs(context).primaryFixed);
+  TextStyle primaryFixedDim(BuildContext context) => copyWith(color: cs(context).primaryFixedDim);
+  TextStyle inversePrimary(BuildContext context) => copyWith(color: cs(context).inversePrimary);
+  TextStyle secondary(BuildContext context) => copyWith(color: cs(context).secondary);
+  TextStyle secondaryContainer(BuildContext context) =>
+      copyWith(color: cs(context).secondaryContainer);
+  TextStyle secondaryFixed(BuildContext context) => copyWith(color: cs(context).secondaryFixed);
+  TextStyle secondaryFixedDim(BuildContext context) =>
+      copyWith(color: cs(context).secondaryFixedDim);
+  TextStyle tertiary(BuildContext context) => copyWith(color: cs(context).tertiary);
+  TextStyle tertiaryContainer(BuildContext context) =>
+      copyWith(color: cs(context).tertiaryContainer);
+  TextStyle tertiaryFixed(BuildContext context) => copyWith(color: cs(context).tertiaryFixed);
+  TextStyle tertiaryFixedDim(BuildContext context) => copyWith(color: cs(context).tertiaryFixedDim);
   TextStyle error(BuildContext context) => copyWith(color: cs(context).error);
+  TextStyle errorContainer(BuildContext context) => copyWith(color: cs(context).errorContainer);
+  TextStyle surface(BuildContext context) => copyWith(color: cs(context).surface);
+  TextStyle surfaceBright(BuildContext context) => copyWith(color: cs(context).surfaceBright);
+  TextStyle surfaceContainer(BuildContext context) => copyWith(color: cs(context).surfaceContainer);
+  TextStyle surfaceContainerHigh(BuildContext context) =>
+      copyWith(color: cs(context).surfaceContainerHigh);
+  TextStyle surfaceContainerHighest(BuildContext context) =>
+      copyWith(color: cs(context).surfaceContainerHighest);
+  TextStyle surfaceContainerLow(BuildContext context) =>
+      copyWith(color: cs(context).surfaceContainerLow);
+  TextStyle surfaceContainerLowest(BuildContext context) =>
+      copyWith(color: cs(context).surfaceContainerLowest);
+  TextStyle surfaceDim(BuildContext context) => copyWith(color: cs(context).surfaceDim);
+  TextStyle surfaceTint(BuildContext context) => copyWith(color: cs(context).surfaceTint);
+  TextStyle inverseSurface(BuildContext context) => copyWith(color: cs(context).inverseSurface);
 
-  TextStyle onPrimery(BuildContext context) =>
-      copyWith(color: cs(context).onPrimary);
-  TextStyle onSecondary(BuildContext context) =>
-      copyWith(color: cs(context).onSecondary);
-  TextStyle onTertiary(BuildContext context) =>
-      copyWith(color: cs(context).onTertiary);
-  TextStyle onBackground(BuildContext context) =>
-      copyWith(color: cs(context).onBackground);
-  TextStyle onSurface(BuildContext context) =>
-      copyWith(color: cs(context).onSurface);
-  TextStyle onError(BuildContext context) =>
-      copyWith(color: cs(context).onError);
+  TextStyle onPrimary(BuildContext context) => copyWith(color: cs(context).onPrimary);
+  TextStyle onPrimaryContainer(BuildContext context) =>
+      copyWith(color: cs(context).onPrimaryContainer);
+  TextStyle onPrimaryFixed(BuildContext context) => copyWith(color: cs(context).onPrimaryFixed);
+  TextStyle onSecondary(BuildContext context) => copyWith(color: cs(context).onSecondary);
+  TextStyle onSecondaryContainer(BuildContext context) =>
+      copyWith(color: cs(context).onSecondaryContainer);
+  TextStyle onSecondaryFixed(BuildContext context) => copyWith(color: cs(context).onSecondaryFixed);
+  TextStyle onTertiary(BuildContext context) => copyWith(color: cs(context).onTertiary);
+  TextStyle onTertiaryContainer(BuildContext context) =>
+      copyWith(color: cs(context).onTertiaryContainer);
+  TextStyle onTertiaryFixed(BuildContext context) => copyWith(color: cs(context).onTertiaryFixed);
+  TextStyle onError(BuildContext context) => copyWith(color: cs(context).onError);
+  TextStyle onErrorContainer(BuildContext context) => copyWith(color: cs(context).onErrorContainer);
+  TextStyle onSurface(BuildContext context) => copyWith(color: cs(context).onSurface);
+  TextStyle onSurfaceVariant(BuildContext context) => copyWith(color: cs(context).onSurfaceVariant);
+  TextStyle onInverseSurface(BuildContext context) => copyWith(color: cs(context).onInverseSurface);
 
   TextStyle color(Color color) => copyWith(color: color);
   TextStyle hexColor(int colorValue) => color(Color(colorValue));
